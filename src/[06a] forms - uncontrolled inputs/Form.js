4f7e063699561d8e2b59524832fbf37b646f83e3
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-06-18 22:41:37 +0200

    .

--- src/[06a] forms - uncontrolled inputs/Form.js
+++ src/[06a] forms - uncontrolled inputs/Form.js
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function Form(props) {
-  let descriptionInputField = React.createRef();
+  let descField = React.createRef();
 
   function submitForm(event) {
     // Prevent that a submit reloads the page.
     event.preventDefault();
 
     // Read the entered description from the textbox
-    const description = descriptionInputField.current.value;
+    const description = descField.current.value;
 
     // Call the addToDo callback function from props and supply the entered description.
     props.addToDo(description);
 
     // Clear the description so the textbox is cleared.
-    descriptionInputField.current.clear();
+    descField.current.clear();
   }
 
   return (
     <form onSubmit={submitForm}>
-      <input type="text" ref={descriptionInputField} />
+      <input type="text" ref={descField} />
       <br />
       <button type="submit">OK</button>
     </form>
