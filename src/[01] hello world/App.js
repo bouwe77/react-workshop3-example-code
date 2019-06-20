485e02ea01d88ae30e6fa73e285970926af991b9
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-06-20 22:27:02 +0200

    .

--- src/[01] hello world/App.js
+++ src/[01] hello world/App.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import "../styles.css";
 
 function App() {
-  return <div>Example 1: Hello World!</div>;
+  return <div>Hello World!</div>;
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-// End of example...
 
+// ================= End of example =======================================
 // Note that the export below is only here to make my Examples App work...
 export default App;
