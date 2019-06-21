01d2bcb7f827959cb298bc3e8ce6f68e07987b34
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-06-21 11:08:25 +0200

    .

--- src/[08] useEffect/App.js
+++ src/[08] useEffect/App.js
@@ -8,12 +8,16 @@ function App() {
   const [renderCount, setRenderCount] = useState(0);
 
   // This effect is called after each render and updates the document title.
+  // The way you call an effect after each render is by NOT supplying a second argument.
   useEffect(() => {
     document.title = `Rendered ${renderCount} times`;
   });
 
-  //TODO uitlezen renderCount uit localStorage
+  //TODO uitlezen renderCount uit localStorage en in state zetten
 
+  // This effect is only called when the renderCount variable from state changes.
+  // You do that by supplying the renderCount variable as seconds argument to useEffect.
+  // What this effect does is saving the renderCount to local storage.
   useEffect(() => {
     console.log("renderCount in useEffect", renderCount);
     localStorage.setItem("renderCount", renderCount);
@@ -22,6 +26,12 @@ function App() {
       localStorage.getItem("renderCount")
     );
   }, [renderCount]);
+
+  // This effect is only called when the components mounts, so that's only once.
+  // To call an effect only once is done by supplying an empty array
+  // as second argument to the useEffect.
+  // What it does is... TBD ;)
+  useEffect(() => {}, []);
 
   function rerenderClick() {
     console.log("renderCount before set", renderCount);
