a05cc87cfccea480ee409cf232a3264a7f508d12
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-06-24 22:07:34 +0200

    .

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header
         nextExample={nextExample}
         prevExample={prevExample}
@@ -55,7 +55,7 @@ function App() {
         hasPrev={hasPrev(currentExampleId)}
       />
       <Example currentExampleId={currentExampleId} />
-    </div>
+    </>
   );
 }
 
